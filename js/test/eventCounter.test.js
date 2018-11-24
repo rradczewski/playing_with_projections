fdaf1b34b050cf6dcab7b91644cac842e0120c3e
Raimo Radczewski <raimoradczewski@googlemail.com>
2018-11-24 14:52:33 +0100

    Add second test verifying the behaviour

--- js/test/eventCounter.test.js
+++ js/test/eventCounter.test.js
@@ -1,5 +1,10 @@
 const eventCounter = require("../src/eventCounter");
+const data0 = require("../../data/0.json");
 
 it("should yield 0 if there are no events", () => {
   expect(eventCounter([])).toEqual(0);
+});
+
+it("should yield the number of events", () => {
+  expect(eventCounter(data0)).toEqual(4);
 });
