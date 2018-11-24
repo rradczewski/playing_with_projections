d94294c61031ac3e46b8dda39647099f9ba725ab
Raimo Radczewski <raimoradczewski@googlemail.com>
2018-11-24 15:46:19 +0100

    popularQuizzes for data5

--- js/src/popularQuizzes.js
+++ js/src/popularQuizzes.js
@@ -1,12 +1,45 @@
 const fold = require("./_fold");
 const pipe = require("./_pipe");
 
+const collectData = fold(
+  (state, event) => {
+    if (event.type === "QuizWasCreated") {
+      return {
+        ...state,
+        quizzes: {
+          ...state.quizzes,
+          [event.payload.quiz_id]: event.payload.quiz_title
+        }
+      };
+    }
+
+    if (event.type === "GameWasOpened") {
+      return {
+        ...state,
+        playCounter: {
+          ...state.playCounter,
+          [event.payload.quiz_id]:
+            (state.playCounter[event.payload.quiz_id] || 0) + 1
+        }
+      };
+    }
+
+    return state;
+  },
+  { quizzes: {}, playCounter: {} }
+);
+
+const flattenQuizTitle = s =>
+  Object.keys(s.playCounter).map(quiz_id => ({
+    quiz_id,
+    quiz_title: s.quizzes[quiz_id],
+    times_played: s.playCounter[quiz_id] || 0
+  }));
+const sortByPlayCount = games =>
+  games.slice().sort((ga, gb) => gb.times_played - ga.times_played);
+
 module.exports = pipe(
-  fold(
-    (state, event) => {
-      return state;
-    },
-    { quizzes: {}, playCounter: {} }
-  ),
-  () => ([])
+  collectData,
+  flattenQuizTitle,
+  sortByPlayCount
 );
--- js/test/popularQuizzes.test.js
+++ js/test/popularQuizzes.test.js
@@ -1,8 +1,21 @@
 const popularQuizzes = require("../src/popularQuizzes");
-const data2 = require("../../data/2.json");
+const data0 = require("../../data/0.json");
+const data5 = require("../../data/5_2015_01_2017_01.json");
 
 describe("should count every time a game was started and yield them by popularity", () => {
   it("should yield [] for no game", () => {
     expect(popularQuizzes([])).toEqual([]);
-  })
+  });
+
+  it("should yield [] if no game was ever played", () => {
+    expect(popularQuizzes(data0)).toEqual([]);
+  });
+
+  it("should yield all quizzes in order of their popularity", () => {
+    expect(popularQuizzes(data5)[0]).toEqual({
+      quiz_id: "362",
+      quiz_title: "C3E87",
+      times_played: 117
+    });
+  });
 });
