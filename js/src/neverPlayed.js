7fe98951b45e06155d2e8cbc4f44717cd113db00
Raimo Radczewski <raimoradczewski@googlemail.com>
2018-11-29 15:01:55 +0100

    Redo implementation for neverPlayed

--- js/src/neverPlayed.js
+++ js/src/neverPlayed.js
@@ -1,9 +1,13 @@
-module.exports = (events) => 
-  events.reduce((playersWhoNeverPlayed, event) => {
-    if(event.type === 'PlayerHasRegistered') {
-      return [...playersWhoNeverPlayed, event.payload];
-    } else if(event.type === 'PlayerJoinedGame') {
-      return playersWhoNeverPlayed.filter(({player_id}) => player_id !== event.payload.player_id);
-    }
-    return playersWhoNeverPlayed;
-  }, []);
+module.exports = (events) => {
+  return events
+    .reduce((playersWhoNeverPlayed, event) => {
+      if(event.type === 'PlayerHasRegistered') {
+        return [...playersWhoNeverPlayed, event.payload];
+      }
+      if(event.type === 'PlayerJoinedGame') {
+        return playersWhoNeverPlayed.filter(({player_id}) => event.payload.player_id !== player_id);
+      }
+      return playersWhoNeverPlayed;
+    }, []);
+};
+
--- js/test/neverPlayed.test.js
+++ js/test/neverPlayed.test.js
@@ -4,52 +4,67 @@ let id = 1;
 const Event = (type, payload) => ({id: `${id++}`, type, timestamp: new Date().toUTCString(), payload});
 
 describe("neverPlayed should list all players who joined but never played", () => {
-  it("should not list a player if they have joined at least one game", () => {
-    const events = [
-      Event("PlayerHasRegistered", { player_id: "1", first_name: "Bla", last_name: "Bla" }),
-      Event("PlayerJoinedGame", { player_id: "1", game_id: "RANDOM_GAME" })
-    ];
-
-    expect(neverPlayed(events)).toEqual([]);
+  it("lists no player if there are none", () => {
+    expect(neverPlayed([])).toEqual([]);
   });
-  
-  it("should list a player if they have not joined at least one game", () => {
+
+  it("lists a single player ", () => {
     const events = [
       Event("PlayerHasRegistered", { player_id: "1", first_name: "Bla", last_name: "Bla" })
     ];
 
-    expect(neverPlayed(events)).toEqual([{player_id: "1", first_name: "Bla", last_name: "Bla"}]);
+    expect(neverPlayed(events)).toEqual([
+      { player_id: "1", first_name: "Bla", last_name: "Bla" }
+    ]);
   });
   
-  it("should only list the player who hasn't played a single game", () => {
-    const events = [
-      Event("PlayerHasRegistered", { player_id: "1", first_name: "Bla", last_name: "Bla" }),
-      Event("PlayerHasRegistered", { player_id: "2", first_name: "Bla", last_name: "Bla" }),
-      Event("PlayerJoinedGame", { player_id: "1", game_id: "RANDOM_GAME" })
-    ];
-
-    expect(neverPlayed(events)).toEqual([{player_id: "2", first_name: "Bla", last_name: "Bla"}]);
-  });
-  
-  it("should only list the player who hasn't played a single game, no matter when they signed up", () => {
-    const events = [
-      Event("PlayerHasRegistered", { player_id: "1", first_name: "Bla", last_name: "Bla" }),
-      Event("PlayerJoinedGame", { player_id: "1", game_id: "RANDOM_GAME" }),
-      Event("PlayerHasRegistered", { player_id: "2", first_name: "Bla", last_name: "Bla" })
-    ];
-
-    expect(neverPlayed(events)).toEqual([{player_id: "2", first_name: "Bla", last_name: "Bla"}]);
-  });
-  
-  it("should list both players if they never played a game", () => {
+  it("lists both players", () => {
     const events = [
       Event("PlayerHasRegistered", { player_id: "1", first_name: "Bla", last_name: "Bla" }),
       Event("PlayerHasRegistered", { player_id: "2", first_name: "Bla", last_name: "Bla" })
     ];
 
     expect(neverPlayed(events)).toEqual([
-      {player_id: "1", first_name: "Bla", last_name: "Bla"}, 
-      {player_id: "2", first_name: "Bla", last_name: "Bla"}
+      { player_id: "1", first_name: "Bla", last_name: "Bla" },
+      { player_id: "2", first_name: "Bla", last_name: "Bla" }
     ]);
   });
+  
+  it("lists a single player who has created a game", () => {
+    const events = [
+      Event("PlayerHasRegistered", { player_id: "1", first_name: "Bla", last_name: "Bla" }),
+      Event("QuizWasCreated", { owner_id: "1", quiz_id: "Q1", quiz_title: "Other event in stream" })
+    ];
+
+    expect(neverPlayed(events)).toEqual([
+      { player_id: "1", first_name: "Bla", last_name: "Bla" }
+    ]);
+  });
+
+  it("lists all players who registered given there were no games", () => {
+    const events = [
+      Event("PlayerHasRegistered", { player_id: "1", first_name: "Bla", last_name: "Bla" }),
+      Event("QuizWasCreated", { owner_id: "1", quiz_id: "Q1", quiz_title: "Other event in stream" }),
+      Event("PlayerHasRegistered", { player_id: "2", first_name: "Bla", last_name: "Bla" })
+    ];
+
+    expect(neverPlayed(events)).toEqual([
+      { player_id: "1", first_name: "Bla", last_name: "Bla" },
+      { player_id: "2", first_name: "Bla", last_name: "Bla" },
+    ]);
+  });
+
+  it("should not list a player who has joined any game", () => {
+    const events = [
+      Event("PlayerHasRegistered", { player_id: "1", first_name: "Bla", last_name: "Bla" }),
+      Event("PlayerJoinedGame", { player_id: "1", game_id: "SOME_GAME" }),
+      Event("PlayerHasRegistered", { player_id: "2", first_name: "Bla", last_name: "Bla" })
+    ];
+
+    expect(neverPlayed(events)).toEqual([
+      { player_id: "2", first_name: "Bla", last_name: "Bla" }
+    ]);
+  });
+
+
 });
